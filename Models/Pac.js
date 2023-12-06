91c1351ce1291a46f36ac0c8bdd1e334bc346fdb
Fabrice Chaplain <75309565+Akrom4@users.noreply.github.com>
2023-12-06 11:49:26 +0100

    "NEW : removing keyEvents, game reset"

--- Models/Pac.js
+++ Models/Pac.js
@@ -16,41 +16,27 @@ class Pac {
     return this.position[1];
   }
 }
+
+/**
+ * View class for the SokoPac game
+ * Handles the user interface rendering
+ */
 class SokoPacView {
   constructor(viewModel) {
-    this.viewModel = viewModel;
-    
+    this.viewModel = viewModel; // Bind the ViewModel to the View
+    this.keyEvent = this.keyEvent.bind(this); // Bind keyEvent to the object, enables the possibility to remove an arrow function from the event listener
     this.initUI();
     this.updateUI();
   }
 
+  // Create the menu and the event listeners
   initUI() {
     document.getElementById("app").innerHTML = this.userInterface();
     this.bindKeyEvents();
     this.addResetEvent();
   }
 
-  bindKeyEvents() {
-    window.addEventListener("keydown", (event) => {
-      switch (event.key) {
-        case "ArrowLeft":
-          this.viewModel.handleMove([-1, 0]);
-          break;
-        case "ArrowRight":
-          this.viewModel.handleMove([1, 0]);
-          break;
-        case "ArrowUp":
-          this.viewModel.handleMove([0, -1]);
-          break;
-        case "ArrowDown":
-          this.viewModel.handleMove([0, 1]);
-          break;
-      }
-    });
-  }
-  removeKeyEvents(){
-    window.removeEventListener("keydown", keyEvent());
-  }
+  // Add the gameMenu and the gameGrid divs
   userInterface() {
     let content = ` <div id="gameInfo">
                         <button id="reset" class="button">Reset</button>
@@ -59,15 +45,45 @@ class SokoPacView {
                   `;
     return content;
   }
+
+  // Keyboard arrows event listeners
+  keyEvent(event) {
+    switch (event.key) {
+      case "ArrowLeft":
+        this.viewModel.handleMove([-1, 0]);
+        break;
+      case "ArrowRight":
+        this.viewModel.handleMove([1, 0]);
+        break;
+      case "ArrowUp":
+        this.viewModel.handleMove([0, -1]);
+        break;
+      case "ArrowDown":
+        this.viewModel.handleMove([0, 1]);
+        break;
+    }
+  }
+
+  // Add the keyboard arrows event listeners
+  bindKeyEvents() {
+    window.addEventListener("keydown", this.keyEvent);
+  }
+
+  // Remove the keyboard arrows event listeners
+  removeKeyEvents() {
+    window.removeEventListener("keydown", this.keyEvent);
+  }
+
+  // Add the reset event listener on the reset button
   addResetEvent() {
     document.getElementById("reset").addEventListener("click", () => {
       this.viewModel.handleReset();
     });
   }
 
+  // Game grid creation and update
   updateUI() {
-    // Get the 'gameGrid' div element
-    const boardDiv = document.getElementById("gameGrid");
+    const boardDiv = document.getElementById("gameGrid"); // Get the 'gameGrid' div element
 
     // Calculate grid size based on the number of squares
     let gridSize = this.viewModel.model.squares[0].length * 5;
@@ -76,67 +92,86 @@ class SokoPacView {
     // Update the inner HTML of the 'gameGrid' div with the current game state
     boardDiv.innerHTML = this.toHTML();
   }
+
+  // Grid rendering
   toHTML() {
     let content = "";
     this.viewModel.model.squares.forEach((row) => {
       row.forEach((square) => {
-        content += ` <div class="squareWrap"><div class="square square${square}"></div></div>`;
+        content += ` <div class="squareWrap"><div class="square square${square}"></div></div>`; // Add the correct css class to a square
       });
     });
     return content;
   }
+
+  // Win handling
   showWinMessage() {
     // Show a win message or screen to the player
     setTimeout(() => {
-        alert("Congratulations! You've won!");
-      }, "100");
+      alert("Congratulations! You've won!");
+    }, "100");
   }
 }
+
+/**
+ * ViewModel for the SokoPac game
+ * Link between Model and View
+ * Retrieves data from Model and exposes it to the View
+ * Retrieves user inputs and exposes it to the Model
+ */
 class SokoPacViewModel {
   constructor(model) {
     this.model = model;
     this.view = null;
-    this.model.setMoveCallback(() => this.onModelUpdated());
-    this.model.setWinCallback(() => this.onModelWin());
+    this.model.setMoveCallback(() => this.onModelUpdated()); // Notifies the ViewModel to update the User Interface
+    this.model.setWinCallback(() => this.onModelWin()); // Notifies the ViewModel that the game is finished
   }
 
+  // Update the UI when the model changes
   onModelUpdated() {
-    // Update the UI when the model changes
     if (this.view) {
       this.view.updateUI();
     }
   }
 
+  // Notify the view that the player has won, remove the keyboard arrow events
   onModelWin() {
-    // Notify the view that the player has won
     if (this.view) {
-        this.view.removeKeyEvents();
+      this.view.removeKeyEvents();
       this.view.showWinMessage();
     }
   }
 
+  // Bind the View to the ViewModel
   setView(view) {
     this.view = view;
   }
 
+  // Move handling
   handleMove(direction) {
     this.model.move(direction);
     this.view.updateUI();
   }
 
+  // Restart the game to it's initial state
   handleReset() {
     this.model.resetBoard();
+    this.view.bindKeyEvents();
     this.view.updateUI();
   }
 }
 
+/**
+ * Model for the SokoPac game
+ * Handles the game logic
+ */
 class SokoPacModel {
   constructor() {
     this.squares = this.initBoard();
     this.winConditionArray = this.winCondition();
   }
 
-  /* Square array 
+  /* Square array legend
         0 -> PacMan
         1 -> Dot
         2 -> Box
@@ -145,18 +180,17 @@ class SokoPacModel {
         5 -> Box on Dot
         6 -> PacMan on Dot
     */
+
+  // Square array that depicts the map
   initBoard() {
     return [
       [3, 3, 3, 3, 3, 3, 3],
       [3, 0, 4, 2, 1, 4, 3],
-      [3, 4, 4, 2, 1, 4, 3],
-      [3, 4, 4, 2, 1, 4, 3],
-      [3, 4, 4, 2, 1, 4, 3],
-      [3, 4, 4, 2, 1, 4, 3],
       [3, 3, 3, 3, 3, 3, 3],
     ];
   }
 
+  // Movement handling
   move(dirMove) {
     // Pacman position To replace when Pac class is implemented
     let rowIndex = this.squares.findIndex((row) => row.includes(0));
@@ -173,17 +207,20 @@ class SokoPacModel {
         let afterNextColumn = nextColumn + dirMove[0];
         let afterNextSquare = this.squares[afterNextRow][afterNextColumn];
 
-        if (afterNextSquare !== 3 && afterNextSquare !== 2 && afterNextSquare !== 5) {
+        if (
+          afterNextSquare !== 3 &&
+          afterNextSquare !== 2 &&
+          afterNextSquare !== 5
+        ) {
           // Move PacMan to the Box's position, and move the Box forward
           this.squares[rowIndex][columnIndex] = 4;
           this.squares[nextRow][nextColumn] = 0;
-          if(afterNextSquare === 1){
+          if (afterNextSquare === 1) {
             this.squares[afterNextRow][afterNextColumn] = 5;
+          } else {
+            this.squares[afterNextRow][afterNextColumn] = 2;
           }
-          else{
-              this.squares[afterNextRow][afterNextColumn] = 2;
-          }
-          
+
           this.onMove();
         }
       } else {
@@ -198,6 +235,7 @@ class SokoPacModel {
     }
   }
 
+  // Win condition check
   checkWinCondition() {
     let win = true;
     this.winConditionArray.forEach((dot) => {
@@ -208,28 +246,30 @@ class SokoPacModel {
     return win;
   }
 
+  // Win condition initialization
   winCondition() {
-    console.log(this.squares.length);
-    console.log(this.squares[0].length);
     let winConditionArray = [];
     for (let i = 0; i < this.squares.length; i++) {
       for (let j = 0; j < this.squares[0].length; j++) {
         if (this.squares[i][j] === 1) {
-          winConditionArray.push([i, j]);
+          winConditionArray.push([i, j]); // Add dots coordinates to the winCondition array
         }
       }
     }
-    console.log(winConditionArray);
     return winConditionArray;
   }
 
+  // Notifies the ViewModel that the game is won
   setWinCallback(callback) {
     this.onWin = callback;
   }
 
+  // Notifies the ViewModel that a move has been played
   setMoveCallback(callback) {
     this.onMove = callback;
   }
+
+  // Restart the game board to its initial state
   resetBoard() {
     this.squares = this.initBoard();
   }
